06c4d8fdf172741d137a139399dc25d0dd0f864c
Reikimann <cbirkmand@gmail.com>
2022-09-18 21:37:48 +0200

    really ignore ignored worsd

--- qtile/autostart.sh
+++ qtile/autostart.sh
@@ -7,6 +7,12 @@
 # Swaps control key and capslock
 setxkbmap -option "ctrl:swapcaps"
 
+# Enable tapping
+xinput set-prop 10 316 1
+
+# Enable tapping
+xinput set-prop 10 308 1
+
 # Launches dxhd
 dxhd -b
 
@@ -18,3 +24,5 @@ feh --randomize --no-fehbg --bg-fill "/home/reikimann/pix/wallpapers"
 
 # Stars dunst
 dunst &
+
+~/.local/bin/host-check
