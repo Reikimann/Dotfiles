c08b6a3c636cddc1bdfc51752892ca832246a71d
Reikimann <cbirkmand@gmail.com>
2023-01-16 22:26:07 +0100

    <html> <head><title>301 Moved Permanently</title></head> <body> <center><h1>301 Moved Permanently</h1></center> </body> </html>

--- hypr/autostart.sh
+++ hypr/autostart.sh
@@ -5,3 +5,6 @@ dunst &
 
 # Sets cursor theme
 hyprctl setcursor Breeze_Snow 16
+
+# Sets cursor theme in gtk apps
+gsettings set org.gnome.desktop.interface cursor-theme "$(grep 'gtk-cursor-theme-name' ~/.config/gtk-3.0/settings.ini | sed 's/.*\s*=\s*//')"
