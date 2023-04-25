1e81b5b3907190a8ba94c089fd40a0b1d3fb6f2c
Reikimann <cbirkmand@gmail.com>
2023-04-25 17:16:13 +0200

    <html> <head><title>301 Moved Permanently</title></head> <body> <center><h1>301 Moved Permanently</h1></center> </body> </html>

--- hypr/autostart.sh
+++ hypr/autostart.sh
@@ -9,3 +9,6 @@ hyprctl setcursor Breeze_Snow 16
 # https://github.com/swaywm/sway/wiki/GTK-3-settings-on-Wayland
 # Sets cursor theme in gtk apps
 gsettings set org.gnome.desktop.interface cursor-theme "$(grep 'gtk-cursor-theme-name' ~/.config/gtk-3.0/settings.ini | sed 's/.*\s*=\s*//')"
+
+# Starts the NetworkManager applet
+nm-applet 2>&1 > /dev/null &
